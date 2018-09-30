fdb2ba38f02adb916f8473ada57f100311bc02ba
rgoyal <rajagoyal815@gmail.com>
2018-09-30 10:12:56 +0530

    initial commit

---
+++ src/main/java/com/example/siddharthtravels/SiddharthtravelsApplication.java
@@ -0,0 +1,12 @@
+package com.example.siddharthtravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiddharthtravelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiddharthtravelsApplication.class, args);
+	}
+}
